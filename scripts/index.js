38f23edf770eaf77e004b8ca5700bd718b234681
Steven Beckham <stevencbeckham@gmail.com>
2023-06-18 23:21:04 -0400

    under construction

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+var gitbutton = document.getElementById('github-button');
+
+gitbutton.addEventListener('click', function() {
+window.location.href = 'https://github.com/Stevey-Becks';
+});
+
+var fbbutton = document.getElementById('facebook-button');
+
+fbbutton.addEventListener('click', function() {
+window.location.href = 'https://www.facebook.com/StevenCBeckham/';
+});
+
+var igbutton = document.getElementById('instagram-button');
+
+igbutton.addEventListener('click', function() {
+window.location.href = 'https://www.instagram.com/steveybecks/';
+});
